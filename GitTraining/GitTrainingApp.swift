4ef373120f2e2f92128a6a247464e810b0b6786e
Jens Rambeck Andersen <86779221+Jenz1000@users.noreply.github.com>
2021-07-07 13:22:05 +0200

    init commit

---
+++ GitTraining/GitTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTrainingApp.swift
+//  GitTraining
+//
+//  Created by Jens Rambeck Andersen on 07/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct GitTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
